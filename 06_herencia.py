55aeac3c0e8fcf1f24f702c7a0c960d938141e28
miller-0509 <caperamiller5@gmail.com>
2026-03-02 17:27:59 -0500

    Create 06_herencia.py

---
+++ 06_herencia.py
@@ -0,0 +1,21 @@
+class Empleado:
+    def __init__(self, nombre: str, salario: float):
+        self.nombre: str = nombre
+        self.salario: float = salario
+    def trabajar(self) -> None:
+        print(f"{self.nombre} está cumpliendo su horario regular.")
+
+class Desarrollador(Empleado):
+    def programar(self) -> None:
+        print(f"{self.nombre} está escribiendo código Python.")
+
+class Gerente(Empleado):
+    def trabajar(self) -> None:
+        print(f"{self.nombre} está en una reunión estratégica.")
+
+dev = Desarrollador("Carlos", 3500.0)
+jeve = Gerente("Ana", 6000.0)
+
+dev.trabajar()
+dev.programar()
+jeve.trabajar()
