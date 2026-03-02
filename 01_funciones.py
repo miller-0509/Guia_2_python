a003242ebc3f9b7259e562bcecf4a576e7ee1db5
miller-0509 <caperamiller5@gmail.com>
2026-03-02 14:45:39 -0500

    Initial commit

---
+++ 01_funciones.py
@@ -0,0 +1,9 @@
+def saludar_usuario() -> None:
+    print("Iniciando conexión con el servidor...")
+    print("Autenticación exitosa. ¡Bienvenido!")
+
+print("---Sistema Principal---")
+
+saludar_usuario()
+saludar_usuario()
+
