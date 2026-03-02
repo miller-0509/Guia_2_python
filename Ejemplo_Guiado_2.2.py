a828c863b3391d4d2670cd92b4b131651f0c9714
miller-0509 <caperamiller5@gmail.com>
2026-03-02 15:25:15 -0500

    Create Ejemplo_Guiado_2.2.py

---
+++ Ejemplo_Guiado_2.2.py
@@ -0,0 +1,6 @@
+def convertir_moneda(cantidad: float, moneda: str = "COP") -> None:
+    print(f"Procesando transacción de {cantidad} en moneda {moneda}")
+
+
+convertir_moneda(5000.0, "USD")   
+convertir_moneda(150000.0)       
